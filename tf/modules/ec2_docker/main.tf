1ebf9d6ba1225d7535632763155671e93a97e5dc
Kortina <kortina@gmail.com>
2020-09-27 21:35:15 -0700

    update ec2 image

--- tf/modules/ec2_docker/main.tf
+++ tf/modules/ec2_docker/main.tf
@@ -51,6 +51,7 @@ sudo apt-get -y install \
     curl \
     gnupg-agent \
     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
