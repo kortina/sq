6b9de5060ec47cf7400c5b3728033a46a9f28abc
Kortina <kortina@gmail.com>
2020-09-28 20:16:12 -0700

    add ssh

--- libsq/dev.py
+++ libsq/dev.py
@@ -7,6 +7,8 @@ from .utils import (
     _running_ec2_docker_public_hostname,
 )
 
+EC2_USER = "ubuntu"  # ec2-user
+
 
 @dev.command(help="Start bash in docker container, inheriting SQ__ prefixed env vars.")
 @click.option("--no-inherit-env", is_flag=True, help="Do NOT inherit SQ__ env vars.")
@@ -35,10 +37,26 @@ def mk_opt_ebs():
 @dev.command(help="Tunnel to the aws docker pg db.")
 @click.argument("host", type=click.STRING, required=False)
 @click.option("--local-port", type=click.STRING, required=False, default="54320")
-def tunnel(host=None, local_port="54320"):
+@click.option(
+    "--print-only", is_flag=True, help="Print cmd (for sending to someone else)."
+)
+def tunnel(host=None, local_port=None, print_only=None):
     if host is None:
         host = _running_ec2_docker_public_hostname()
 
-    user = "ubuntu"
+    user = EC2_USER
+    info = "---------------- local_port:remote_addr:remote_port remote_user@remote_host ----------------"
     cmd = f"ssh -vvv -TnN -L {local_port}:0.0.0.0:5432 {user}@{host}"
+    if print_only:
+        print(info)
+        print(cmd)
+    else:
+        _run_command(cmd, capture_output=False)
+
+
+@dev.command(help="ssh to the aws docker pg db.")
+def ssh():
+    host = _running_ec2_docker_public_hostname()
+    user = EC2_USER
+    cmd = f"ssh {user}@{host}"
     _run_command(cmd, capture_output=False)
